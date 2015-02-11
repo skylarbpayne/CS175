1752667b668312c71c60e949983dba60d306d1bb
Skylar Payne <skylar.b.payne@gmail.com>
2015-02-11 09:45:48 -0800

    Made a change to load_tweets to filter out non-tweet elements

--- load_tweets.py
+++ load_tweets.py
@@ -3,4 +3,4 @@ import json
 def load_tweets(fn: str) -> list:
     with open(fn) as f:
         content = f.readlines()
-        return [json.loads(s) for s in content]
+        return [t for t in [json.loads(s) for s in content] if 'text' in t]
