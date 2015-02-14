1e7e588a12f164aa690e1525114b2b8fc9f7bea4
Skylar Payne <skylar.b.payne@gmail.com>
2015-02-14 14:48:08 -0800

    Added binary feature extractor reply

--- feature_extractors.py
+++ feature_extractors.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
 def word_counts(tweets: list) -> (np.ndarray, list):
@@ -5,6 +6,7 @@ def word_counts(tweets: list) -> (np.ndarray, list):
     Takes a list of dictionary objects representing tweets. Returns a sparse word count matrix of size (num tweets) by (num words)
 
     :param tweets: The list of tweets, where each tweet is a dictionary
+    
     :return: tuple of (count matrix, vocabulary list)
     '''
     
@@ -17,15 +19,32 @@ def num_hashtags(tweets: list) -> list:
     Takes a list of dictionary objects representing tweets. Returns the number of hashtag counts for each tweet
 
     :param tweets: The list of tweets, where each tweet is a dictionary
+    
     :return: list of hashtag counts
     '''
 
     return [t['text'].count('#') for t in tweets]
 
-if __name__ == '__main__':
-    tweets = [{'text': 'hello #whatup #you #me'}]
-    
-    assert(num_hashtags(tweets)[0] == 3)
+def reply(tweets: list) -> list:
+    '''
+    Takes a list of dictionary objects representing tweets. Returns a binary feature of whether or not the tweet is a reply
 
+    :param tweets: The list of tweets, where each tweet is a dictionary
+    
+    :return: list of binary elements; 1 = is a reply, 0 = not a reply
+    '''
+    
+    return[1 if t['in_reply_to_user_id'] != None else 0 for t in tweets]
+
+if __name__ == '__main__':
+    tweets = [{'text': 'hello #whatup #you #me', 'in_reply_to_user_id': None}]
+    
+    #test num_hashtags
+    assert(num_hashtags(tweets)[0] == 3)
+    
+    #test word_counts
     assert(word_counts(tweets)[0].nnz == 4)
     assert(len(word_counts(tweets)[1]) == 4)
+
+    #test reply
+    assert(reply(tweets)[0] == 0)
