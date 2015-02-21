821a0f0fa51b608b46f0f0a10cf5f27e92786d3c
Kenneth Baldauf <yankeeken@gmail.com>
2015-02-20 18:48:06 -0800

    Fixes #7: Changed input to dictionary.

--- remove_stop_punctuation.py
+++ remove_stop_punctuation.py
@@ -3,24 +3,26 @@ import string
 punc = string.punctuation.replace('#', '')
 english_stop_words = nltk.corpus.stopwords.words('english') + ['']
 
-def remove_stop_punctuation(tokenized_tweets: list) -> list:
+def remove_stop_punctuation(tweets: list) -> list:
     '''
-        tweets is a list of tokenized tweets (list of list of tokens).
+        tweets is a list of dictionaries representing tweet objects
         The output of this transformer is a list with all the stop words and punctuation removed.
     '''
-    return[[token.strip(punc) for token in tweet if token.strip(punc).lower() not in english_stop_words] for tweet in tokenized_tweets]
-    
+    for tweet in tweets:
+        tweet['text'] = ' '.join([token.strip(punc) for token in tweet['text'].split() if token.strip(punc).lower() not in english_stop_words])
+    return tweets
     
 if __name__ == '__main__':
     '''This runs some simple unit tests to ensure that the transformer works correctly'''
 
-    test_tweets = [['Hey','There,','!'],['apple','orange','test'],['Website.']]
+    test_tweets = [{'text':'Hey There, !'}, {'text':'#apple orange test'}, {'text':'Website.', 'in_reply_to_user_id': None}]
     t1 = remove_stop_punctuation(test_tweets)
     print('Contents of test_tweets:', test_tweets)
-    assert(len(t1[0]) == 1)
+    assert(len(t1[0]['text'].split()) == 1)
     print('t1[0]:', t1[0])
-    assert(len(t1[1]) == 3)
+    assert(len(t1[1]['text'].split()) == 3)
     print('t1[1]:', t1[1])
-    assert(len(t1[2]) == 1)
-    assert(t1[2][0] == 'Website')
+    assert(len(t1[2]['text'].split()) == 1)
+    assert(t1[2]['text'] == 'Website')
+    assert(len(t1[2]) == 2)
     print('t1[2]:', t1[2])
