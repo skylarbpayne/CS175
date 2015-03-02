a63a3ddcd364d6493a6ef985c45eb3dae6ecbceb
Kenneth Baldauf <yankeeken@gmail.com>
2015-03-01 17:52:11 -0800

    Fixed some minor spelling errors in clustering.py

--- clustering.py
+++ clustering.py
@@ -15,11 +15,11 @@ def complete_linkage_clustering(Xtr: np.ndarray, k: int) -> np.ndarray:
     :return: the cluster assignments for each data point
     '''
     complete = AgglomerativeClustering(n_clusters = k, linkage='complete')
-    return iomplete.fit_predict(Xtr.toarray())
+    return complete.fit_predict(Xtr.toarray())
 
 def ward_linkage_clustering(Xtr: np.ndarray, k: int) -> np.ndarray:
     '''
-    Performs hierarchical clustering with complete linkage
+    Performs hierarchical clustering with ward linkage
 
     :param Xtr: the observation x feature matrix
     :param k: the number of clusters
