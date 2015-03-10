a9e40f628ddf7eb6d12aa619f071606f938476ae
Skylar Payne <skylar.b.payne@gmail.com>
2015-03-10 00:55:56 -0700

    Started performing all pre-trend extraction features with all data--to provide a better idea of the use of words on average--better trend extraction

--- pipeline.py
+++ pipeline.py
@@ -10,27 +10,52 @@ from clustering import *
 from trend_extraction import *
 
 import json
+from scipy import sparse
+
 
 if __name__ == '__main__':
-    tweets = load_tweets('data/sample1.txt')
+    print('Loading...')
+    samples = [load_tweets('data/sample%d.txt' % i) for i in range(1,7)]
+    sample1_ids = set([tw['id'] for tw in samples[0]])
+    tweets = np.concatenate(samples)
+    print('Loaded.')
+    
+    print('Cleaning...')
     tweets = remove_stop_words(tweets)
     tweets = remove_punctuation(tweets)
     tweets = remove_non_english(tweets, 0.2)
     tweets = remove_links(tweets)
     tweets = remove_digits(tweets)
     tweets = remove_empty(tweets)
-    feats, vocab = tf_idf(tweets, None, 0.994, 0.006)
+    print('Cleaned.')
 
-    print(feats.shape)
-    clusters = complete_linkage_clustering(feats, 120)
+    #By using all tweets, we get a better idea of what the "true" use of the word is. Can detect spikes!
+    print('Extracting Features...')
+    feats, vocab = tf_idf(tweets, None, .995, 0.005)
+    print('Features Extracted.')
+
+    #filter out tweets not in sample1
+    print('Filtering...')
+    sample1_feats = feats[np.array([i for i in range(len(tweets)) if tweets[i]['id'] in sample1_ids]), :]
+    print('Filtered')
     
+    print('\tfull feats:', feats.shape)
+    print('\tsample1_feats shape:', sample1_feats.shape)
+
+    print('Clustering...')
+    clusters = complete_linkage_clustering(sample1_feats, 120)
+    print('Clustered')
+
 ##Largest cluster?
+    print('Getting largest cluster...')
     largest_cluster_ind = np.argmax([sum(clusters == i) for i in range(20)])
-    largest_cluster = feats[clusters==largest_cluster_ind,:]
-    
+    largest_cluster = sample1_feats[clusters==largest_cluster_ind,:]
+    print('Found largest cluster.')
+
     #with open('data/sample1.trends.txt') as f:
     #    content = f.readlines()
     #    trends = [t['name'] for t in [json.loads(s)['trends'] for s in content] if 'name' in t]
 
-
+    print('Extracting trends...')
     print(lda_extract_topic(largest_cluster, vocab, 30))
+    print('Trends extracted.')
