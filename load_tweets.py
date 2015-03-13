bbb987855d55ef56e7d60266126f391c1f160caf
Kenneth Baldauf <yankeeken@gmail.com>
2015-03-13 01:28:24 -0700

    Added newline check to ensure load_tweets.py will work on python

--- load_tweets.py
+++ load_tweets.py
@@ -3,4 +3,4 @@ import json
 def load_tweets(fn: str) -> list:
     with open(fn) as f:
         content = f.readlines()
-        return [t for t in [json.loads(s) for s in content] if 'text' in t]
+        return [t for t in [json.loads(s) for s in content if s != '\n'] if 'text' in t]
